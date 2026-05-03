1283168d8cd817406b1638d7202ffe88b6217822
Sam Mooney <sammooneydev@gmail.com>
2026-05-03 14:07:47 +0100

    implemented first card on dashboard to show most popular movie currently

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from dash import Dash, html
+from ingestion import fetch_popular
+
+#fetching most popular film from API
+movie = fetch_popular()
 
 app = Dash()
 
@@ -12,8 +16,28 @@ app.layout = html.Div([
         )
     ]),
 
-    #content could go here (undecided)
-    html.Div([])
+    #most popular movie right now
+    html.Div([html.H2("Current Most Popular Movie"),
+
+    html.Img(
+        src=movie["poster_url"],
+        style={'width': '200px'}
+    ),
+
+    html.H3(movie["title"]),
+
+    html.P(f"Average Rating: {movie['rating']}"),
+
+    html.P(f"Last updated: {movie['last_updated']}")
+    ], 
+    style={
+    'backgroundColor': '#2a2a2a',
+    'padding': '10px',
+    'borderRadius': '6px',
+    'textAlign': 'center',
+    'width': '250px',
+    'margin': '20px auto'
+    })
 ])
 
 if __name__ == '__main__':
--- ingestion.py
+++ ingestion.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import time
 from transform_data import clean_movies
+from datetime import datetime
 
 #pulling API key from .env file
 load_dotenv()
@@ -15,20 +16,23 @@ BASE_URL = "https://api.themoviedb.org/3"
 #passing API key 
 headers = {'Authorization': f"Bearer {API_KEY}"}
 
-def fetch_movies(endpoint, pages):
-    all_movies = []
+def fetch_popular():
+    url = f"{BASE_URL}/movie/popular?page=1" #getting first page of most popular movies
     
-    for page in range(1, pages + 1):
-        url = f"{BASE_URL}{endpoint}?page={page}"
-        
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        
-        all_movies.extend(data.get("results", []))
-        
-        #for rate limiting
-        time.sleep(0.2)
+    response = requests.get(url, headers=headers)
+    data = response.json()
     
-    return all_movies
-
-movies = fetch_movies("/movie/popular", pages = 3)
+    movies = data.get("results", [])
+    
+    if not movies:
+        return None
+    
+    #assigning top_movie to first result on popular page, as this is the current top movie
+    top_movie = movies[0]
+    
+    return {
+        "title": top_movie.get("title"),
+        "rating": top_movie.get("vote_average"),
+        "poster_url": f"https://image.tmdb.org/t/p/w500{top_movie.get("poster_path")}", #creating poster url from path returned by API
+        "last_updated": datetime.now().strftime("%d-%m-%Y %H:%M")
+    }
